be0d6e4861c37543880e593b3c60daa71633e182
RASantos92 <rasbms81608@gmail.com>
2023-03-13 15:51:38 -0500

    3/13 more forms

--- sessionformdemo/src/main/java/com/robert/sessionformdemo/controllers/HomeController.java
+++ sessionformdemo/src/main/java/com/robert/sessionformdemo/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -41,14 +42,26 @@ public class HomeController {
 	}
 	
 	@GetMapping("/review/form")
-	public String renderReviewForm() {
+	public String renderReviewForm(Model model) {
+		model.addAttribute("name", "Abrar");
 		return "postForm/form.jsp";
 	}
 	
 	@PostMapping("/process/review/form")
-	public String processReviewForm(@RequestParam("movie") String movie,@RequestParam("comment") String comment, @RequestParam("rating") Integer rating,HttpSession session) {
+	public String processReviewForm(@RequestParam("movie") String movie,@RequestParam("comment") String comment, @RequestParam("rating") Integer rating, HttpSession session, RedirectAttributes redirectAttributes, @RequestParam("reviewer") String reviewer) {
+		System.out.println(movie );
+		
+		if(movie == "") {
+			redirectAttributes.addFlashAttribute("error", "movie must not be null");
+			return "redirect:/review/form";
+		}
+		if(rating < 0 || rating > 10) {
+			redirectAttributes.addFlashAttribute("error", "Rating must be between 0 - 10");
+			return "redirect:/review/form";
+		}
 		System.out.println("test");
-		session.setAttribute("movie", movie);
+		session.setAttribute("movie", movie);	
+		session.setAttribute("reviewer", reviewer);
 		session.setAttribute("comment", comment);
 		session.setAttribute("rating", rating);
 		return "redirect:/display/review/form";
--- sessionformdemo/src/main/webapp/WEB-INF/postForm/display.jsp
+++ sessionformdemo/src/main/webapp/WEB-INF/postForm/display.jsp
@@ -10,6 +10,7 @@
 <body>
 	    <h1> Review info </h1>
         <h3>Movie: <c:out value="${movie}"/></h3>
+        <h3>Reviewer: <c:out value="${reviewer}"/></h3>
         <h3>comment: <c:out value="${comment}"/></h3>
         <h3>rating: ${rating}</h3>
 </body>
