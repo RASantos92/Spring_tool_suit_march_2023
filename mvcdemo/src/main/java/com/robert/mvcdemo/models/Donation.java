34dc9e255805444344ce7d2be925e4312e2bdc89
RASantos92 <rasbms81608@gmail.com>
2023-03-14 12:11:12 -0500

    3/14 mvc intro

---
+++ mvcdemo/src/main/java/com/robert/mvcdemo/models/Donation.java
@@ -0,0 +1,111 @@
+package com.robert.mvcdemo.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="donations")
+public class Donation {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min=3, max=255, message="donation name needs to be more than 3 charaters")
+	@NotEmpty(message="please provide a donation name")
+	private String donationName;
+	
+	@Size(min=2, max=50)
+	@NotEmpty(message="donor name should not be empty")
+	private String donor;
+	
+	@Min(0)
+	@NotNull
+	private Integer quantity;
+	
+	@Column(updatable = false)
+	private Date createdAt;
+	
+	@Column(updatable = false)
+	private Date updatedAt;
+
+	public Donation() {	
+	}
+	
+
+	public Donation(String donationName, String donor, Integer quantity) {
+		this.donationName = donationName;
+		this.donor = donor;
+		this.quantity = quantity;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDonationName() {
+		return donationName;
+	}
+
+	public void setDonationName(String donationName) {
+		this.donationName = donationName;
+	}
+
+
+	public String getDonor() {
+		return donor;
+	}
+
+
+	public void setDonor(String donor) {
+		this.donor = donor;
+	}
+
+
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+
+}
